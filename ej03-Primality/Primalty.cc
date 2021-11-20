ae47c3dcb121cc9498673e8a0e299c2e3af974b2
Alexander <alu0101463018@ull.edu.es>
2021-11-20 22:01:41 +0000

    ejercicio 3

--- ej03-Primality/Primalty.cc
+++ ej03-Primality/Primalty.cc
@@ -1,45 +1,46 @@
 #include <iostream>
-/*
-void GenerateNumbers(int number) {
-  std::cin >> number;
-  int arr[number];
-  for (int i = 0; i < number; i++) 
-  std::cin >> arr[i];
-}
-*/
+
 void PrimeNumbers(int natural_numbers) {
-  int number{0},count{0};
+  int count{0};
   bool isPrime = true;
 
-  int arr[number];
-  for (int i = 0; i < number; i++) 
-  // std::cin >> arr[i];
-  for (int j = 2; j <= natural_numbers/2; j++) {
+  for (int j = 2; j <= natural_numbers / 2; j++) {
     if (natural_numbers % j == 0) {
       count = 1;
       break;
     }
   }
-
-  if (natural_numbers == 1 || natural_numbers == 0) {
+if (natural_numbers == 1 || natural_numbers == 0) {
+  if (natural_numbers == 1) { 
     isPrime = false;
-
+    std::cout << "1 is a not prime number" << std::endl;
+  
+  } else if (natural_numbers == 0) {
+    isPrime = false;
+    std::cout << "0 is a not prime number" << std::endl;
+  }
   } else {
-      if (count == 0) {
-        std::cout << natural_numbers << " is a prime number" << std::endl;
-      } else {
-        std::cout << natural_numbers << " is a not prime number" <<std::endl;
-      }
+    if (count == 0) {
+      std::cout << natural_numbers << " is a prime number" << std::endl;
+    } else {
+      std::cout << natural_numbers << " is not a prime number" << std::endl;
     }
+  }
 }
 
+int main() {
+  int number{0}, i{0},n{0};
+  // std::cout << "How many numbers would you loke to introduce:";
+  std::cin >> number;
+  int natural_numbers[number];
+  // std::cout << "Introduzca los número que desee:";
 
-    int main() {
-      int natural_numbers{0}, number{0}, count{0};
-      std::cin >> number;
-      std::cin >> natural_numbers;
-      // std::cout << "How many numbers would you loke to introduce:"
-      PrimeNumbers(natural_numbers);
+  for (i = 0; i < number; i++ ) {
+    std::cin >> natural_numbers[i];
+    PrimeNumbers(natural_numbers[i]);
 
-      return 0;
-    }
+  }
+  
+  
+  return 0;
+}
