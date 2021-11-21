c69ed2625e7340ad6ed4f2bcbdf2e690dbaf228d
Alexander <alu0101463018@ull.edu.es>
2021-11-21 18:45:34 +0000

    ejercicio 3

--- ej03-Primality/Primality.cc
+++ ej03-Primality/Primality.cc
@@ -1,3 +1,16 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Alexander Morales Díaz
+ * @date 19.nov.2021
+ * @brief Practica 8. Ejercicio 2
+ * Introduce different numbers and it will tell you if they are or not primes
+ */
+
+
 #include <iostream>
 #include <cmath>
 
