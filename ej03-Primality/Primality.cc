4d52f4a1f22781b6451051d09e2a6e3f4d232ed6
Alexander <alu0101463018@ull.edu.es>
2021-11-21 19:04:37 +0000

    ejercicio 3

--- ej03-Primality/Primality.cc
+++ ej03-Primality/Primality.cc
@@ -11,34 +11,47 @@
  */
 
 
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2021-2022
+  *
+  *
+  * @author Alexander Morales Díaz
+  * @date Nov 18 2021
+  * @brief El programa lee una secuencia de numeros naturales e imprime para cada uno
+  *        si es primo o no.
+  */
+
 #include <iostream>
 #include <cmath>
 
-bool Prime(const int number) {
-
-bool isPrime = true;
-if (number == 1 || number == 0) {
-  return isPrime = false;
-}
-for (int i = 2; i <= sqrt (number); i++) {
-  if (number % i == 0) {
-    isPrime = false;
+bool Prime(const int kNumeroNatural) {
+  bool isPrime = true;
+  if (kNumeroNatural == 1 ||kNumeroNatural == 0) {
+    return isPrime = false;
   }
-}
+  for (int i = 2; i <= sqrt(kNumeroNatural); ++i) {
+    if (kNumeroNatural % i == 0) {
+      return isPrime = false;
+    }
+  }
   return isPrime;
 }
 
-int main () {
-  int amount_numbers{0}, natural_number{0};
+int main() {
+  int amount_numbers(0), natural_number(0);
   std::cin >> amount_numbers;
-
-  for (int i = 1; i <= amount_numbers; i++) {
+  
+  for (int i = 1; i <= amount_numbers; ++i) {
     std::cin >> natural_number;
+
     if (Prime(natural_number) == true) {
       std::cout << natural_number << " is prime" << std::endl;
     } else {
       std::cout << natural_number << " is not prime" << std::endl;
-    }
+    }    
   }
   return 0;
 }
