62844a431875e950c6bd9969024198450832324e
Alexander <alu0101463018@ull.edu.es>
2021-11-21 18:43:56 +0000

    ejercicio 3

--- ej03-Primality/Primality.cc
+++ ej03-Primality/Primality.cc
@@ -1,55 +1,31 @@
-/**
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Informática Básica
- *
- * @author Alexander Morales Díaz
- * @date 19.nov.2021
- * @brief Practica 8. Ejercicio 3
- * Introduce different and it will say if they are or no primes.
- */
 #include <iostream>
+#include <cmath>
 
-void PrimeNumbers(int natural_numbers) {
-  int count{0};
-  bool isPrime = true;
+bool Prime(const int number) {
 
-  for (int j = 2; j <= natural_numbers / 2; j++) {
-    if (natural_numbers % j == 0) {
-      count = 1;
-      break;
-    }
-  }
-  if (natural_numbers == 1 || natural_numbers == 0) {
-    if (natural_numbers == 1) {
-      isPrime = false;
-      std::cout << "1 is not prime " << std::endl;
-
-    } else if (natural_numbers == 0) {
-      isPrime = false;
-      std::cout << "0 is not prime " << std::endl;
-    }
-  } else {
-    if (count == 0) {
-      std::cout << natural_numbers << " is prime number" << std::endl;
-    } else {
-      std::cout << natural_numbers << " is not prime" << std::endl;
-    }
+bool isPrime = true;
+if (number == 1 || number == 0) {
+  return isPrime = false;
+}
+for (int i = 2; i <= sqrt (number); i++) {
+  if (number % i == 0) {
+    isPrime = false;
   }
 }
+  return isPrime;
+}
 
-int main() {
-  int number{0}, i{0}, n{0};
-  // std::cout << "How many numbers would you loke to introduce:";
-  std::cin >> number;
-  int natural_numbers[number];
-  // std::cout << "Introduzca los número que desee:";
+int main () {
+  int amount_numbers{0}, natural_number{0};
+  std::cin >> amount_numbers;
 
-  for (i = 0; i < number; i++) {
-    std::cin >> natural_numbers[i];
-    PrimeNumbers(natural_numbers[i]);
+  for (int i = 1; i <= amount_numbers; i++) {
+    std::cin >> natural_number;
+    if (Prime(natural_number) == true) {
+      std::cout << natural_number << " is prime" << std::endl;
+    } else {
+      std::cout << natural_number << " is not prime" << std::endl;
+    }
   }
-
   return 0;
 }
